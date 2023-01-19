b40197685132b80d674411ac49cefdbdfb96eeaa
Satrey <satrey.mail@gmail.com>
2023-01-19 13:34:27 +0500

    Добавлен login view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -9,4 +9,13 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,)
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return HttpResponse('Authentication successful')
+                else:
+                    return HttpResponse('Authentication failed')
+        else:
+            form = LoginForm()
+            return render(request, 'account/login.html', {'form': form})
