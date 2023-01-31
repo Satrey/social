d5e4665f8619423bb0d4b830deb34da7a55ed8d4
Александр <satrey.mail@gmail.com>
2023-02-01 03:21:10 +0500

    Доработан login.html стр. 157 Стили формы

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -4,3 +4,5 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput, max_length=30)
+    required_css_class = "field"
+    error_css_class = "error"
